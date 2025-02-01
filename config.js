455d5d66e9c9b9e81bc862c6e601a2e2d5876d6f
Hayssa5 <sam.mezher@outlook.com>
2025-02-01 07:58:05 +0100

    Fixade error med config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
-require('dotenv').config = {
+const config = {
   apiKey: 'AIzaSyBRXDDuGIfObeQNtasWSTFTHx1X33b6A2Y', // Lägg din API-nyckel här
 };
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBRXDDuGIfObeQNtasWSTFTHx1X33b6A2Y',
+  apiKey: config.apiKey,
   authDomain: 'testeventmap-ffe44.firebaseapp.com',
   databaseURL:
     'https://testeventmap-ffe44-default-rtdb.europe-west1.firebasedatabase.app',
--- script.js
+++ script.js
@@ -424,7 +424,7 @@ const qrcodeContainer = document.getElementById('qrcode');
 console.log(qrcodeContainer);
 
 // Skapa en QR-kod med rätt metod
-QRCode.toCanvas(qrcodeContainer, 'https://hayssa5.github.io/EventMap/', {
+QRCode.toCanvas(qrcodeContainer, ' https://hayssa5.github.io/EventMap/', {
   width: 128, // Bredd på QR-koden
   color: {
     dark: '#000000', // Färg på QR-koden
